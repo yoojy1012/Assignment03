97fc1332fd64355eeea516fceec15c6076c85e27
yoojy1012 <55114506+yoojy1012@users.noreply.github.com>
2019-10-06 23:48:58 +0900

    2

--- Project4/subset.c
+++ Project4/subset.c
@@ -2,22 +2,30 @@
 
 char subset(char);
 
-int i;
+int i, level;
+char element;
 
 int main(int argc, char* argv[])
 {
+	level = argc;
+	element = *argv;
 
-	for (i = 0; i <= argc - 1; i++)
-		
+	for (i = 0; i < argc; i++)
+
 		printf("[%04d] %s\n", i + 1, subset(i));
-	
+
 	return 0;
 }
 
-char subset(char sub)
+char subset(int sub)
 {
 	if (sub == 0)
 		return 155;
+
 	else
-		return 
+		for (i = 1; i <= level; i++)
+		{
+			return ()
+		}
+
 }
